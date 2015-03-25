324b7ec0cf1f85c0b5e7f8ba7138c6f995af1b95
mseshachalam <seshachalam@outlook.com>
2015-03-25 19:30:09 +0530

    with httprouter

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 // go-angular project main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
 
 func main() {
 	fmt.Printf("hello, world\n")
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ photo.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Image sizes supported by Flickr.  See
+// http://www.flickr.com/services/api/misc.urls.html for more information.
+const (
+	SizeSmallSquare = "s"
+	SizeThumbnail   = "t"
+	SizeSmall       = "m"
+	SizeMedium500   = "-"
+	SizeMedium640   = "z"
+	SizeLarge       = "b"
+	SizeOriginal    = "o"
+)
+
+// Response for photo search requests.
+type SearchResponse struct {
+	Page    string  `xml:"page,attr"`
+	Pages   string  `xml:"pages,attr"`
+	PerPage string  `xml:"perpage,attr"`
+	Total   string  `xml:"total,attr"`
+	Photos  []Photo `xml:"photo"`
+}
+
+// A Flickr user.
+type User struct {
+	UserName string `xml:"username,attr"`
+	NSID     string `xml:"nsid,attr"`
+}
+
+// Represents a Flickr photo.
+type Photo struct {
+	ID       string `xml:"id,attr"`
+	Owner    string `xml:"owner,attr"`
+	Secret   string `xml:"secret,attr"`
+	Server   string `xml:"server,attr"`
+	Farm     string `xml:"farm,attr"`
+	Title    string `xml:"title,attr"`
+	IsPublic string `xml:"ispublic,attr"`
+	Width_T  string `xml:"width_t,attr"`
+	Height_T string `xml:"height_t,attr"`
+	// Photo's aspect ratio: width divided by height.
+	Ratio float64
+}
+
+// Returns the URL to this photo in the specified size.
+func (p *Photo) URL(size string) string {
+	if size == "-" {
+		return fmt.Sprintf("http://farm%s.static.flickr.com/%s/%s_%s.jpg",
+			p.Farm, p.Server, p.ID, p.Secret)
+	}
+	return fmt.Sprintf("http://farm%s.static.flickr.com/%s/%s_%s_%s.jpg",
+		p.Farm, p.Server, p.ID, p.Secret, size)
+}
+
+type PhotoSet struct {
+	ID          string `xml:"id,attr"`
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+}
