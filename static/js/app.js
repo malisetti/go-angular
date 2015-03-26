3f3fe42a0872c04ba73800d69bd1379a9d4b7214
mseshachalam <seshachalam@outlook.com>
2015-03-27 00:44:51 +0530

    making put calls

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,9 @@
               loading: false
             };
 
+            $scope.upvotes = [];
+            $scope.downvotes = [];
+
             $scope.response = {}
             ,$scope.response.page = 0
             ,$scope.response.pages = 0
@@ -36,6 +39,20 @@
               });
             };
 
+            $scope.votePuppy = function(vote){
+              var url = "/pups";
+
+              $http.put(url, vote).
+              success(function (data, status, headers, config) {
+                alert(status);
+              }).
+              error(function (data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                $scope.main.page--;
+              });
+            };
+
             $scope.open = function (pup) {
               var newScope = $scope.$new();
               newScope.pup = pup;
@@ -48,11 +65,32 @@
             };
 
             $scope.upvote = function(pup) {
+              var alreadyUpVoted = $scope.upvotes.indexOf(pup.id);
+              var alreadyDownVoted = $scope.downvotes.indexOf(pup.id);
+              if(alreadyUpVoted * alreadyDownVoted == 1){
+                $scope.upvotes.push(pup.id);
+                pup.upvotes++
 
+                var vote = {};
+                vote.ID = pup.id;
+                vote.VT = true;
+
+                $scope.votePuppy(vote);
+              }
             }
 
-            $scope.downvote = function() {
+            $scope.downvote = function(pup) {
+              var alreadyUpVoted = $scope.upvotes.indexOf(pup.id);
+              var alreadyDownVoted = $scope.downvotes.indexOf(pup.id);
+              if(alreadyUpVoted * alreadyDownVoted == 1){
+                $scope.downvotes.push(pup.id);
+                pup.downvotes++
+                var vote = {};
+                vote.ID = pup.id;
+                vote.VT = false;
 
+                $scope.votePuppy(vote);
+              }
             }
 
 
